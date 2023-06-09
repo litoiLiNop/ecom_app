9b074b67fe3db1528a2750d524d548c86d8c5f84
litoilinop <litoilinop@gmail.com>
2023-06-09 17:34:15 +0100

    review_1

--- database/migrations/2023_06_09_155348_create_reviews_table.php
+++ database/migrations/2023_06_09_155348_create_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,19 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->string('comment');
+            $table->string('rating');
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->string('status')->default(0);
+            $table->integer('vendor_id')->nullable();
             $table->timestamps();
         });
     }
